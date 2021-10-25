a1020986050466ab9a36ee11a3461b332e34f3ee
zzt <dyxzzt1108@163.com>
2021-10-26 02:23:02 +0800

    fix: 下载问题

--- src/utils/invoke.js
+++ src/utils/invoke.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 import { localStorageGet, messageInfo } from './util'
 import qs from 'qs'
+import _ from 'lodash'
 
 // 重试上限次数
 axios.defaults.retry = 4
@@ -24,8 +25,39 @@ export default function request(option) {
     },
     error => Promise.reject(error))
 
-		instance.interceptors.response.use(response => {
-			return response.data
+		instance.interceptors.response.use((response) => {
+			const data = response.data
+			const code = response.status
+			const headers = response.headers
+			const message = response.message || ''
+			console.log('11', headers['content-type'].indexOf('application/vnd.ms-excel'))
+			if(code !== 200){
+				// 由于此处无法引入element-ui,暂时打印处理
+				console.log('error', message)
+			} else if (headers['content-type'].indexOf('application/vnd.ms-excel') > -1) {
+
+				// 文件流处理 下载文件
+				const content = headers['content-disposition'];
+				// 获取文件名称
+				let file_name = (_.split(content, 'fileName='))[1]
+				// 解码
+				file_name = decodeURIComponent(file_name)
+
+				if ('download' in document.createElement('a')) { // 非IE下载
+					const url = window.URL.createObjectURL(data)
+					const link = document.createElement('a')
+					link.href = url
+					link.setAttribute('download', file_name)
+					document.body.appendChild(link)
+					link.click()
+					window.URL.revokeObjectURL(url);
+					document.body.removeChild(link)
+				} else { // IE 10+ 下载
+					navigator.msSaveBlob((data), file_name)
+				}
+			} else {
+				return data
+			}
 		}, err => {
       console.log(err);
       if (err && err.response) {
--- src/views/chapter/manage.vue
+++ src/views/chapter/manage.vue
@@ -121,7 +121,7 @@
                       <!-- eslint-disable-next-line -->
                       <template slot-scope="scope">
                         <button type="button" class="btn btn-sm btn-success waves-effect waves-light m-1" @click="dialogchapers = true">导入</button>
-                        <button type="button" class="btn btn-sm btn-success btn-outline waves-effect waves-light m-1">导出</button>
+                        <button type="button" class="btn btn-sm btn-success btn-outline waves-effect waves-light m-1" @click="getDownload()">导出</button>
                       </template>
                     </el-table-column>
                   </el-table-column>
@@ -181,7 +181,9 @@
 </template>
 
 <script>
+import { exportExcel } from '@/api/activity'
 import request from '../../utils/request'
+
 export default {
   name: 'ManageChapter',
   data() {
@@ -236,6 +238,12 @@ export default {
     }
   },
   methods: {
+    // 点击触发下载，逻辑在invoke.js
+    getDownload() {
+      exportExcel({ activityId: '201812011402573846878210012018' }).then((res) => {
+        console.log(res)
+      })
+    },
     // 每页条数改变时触发 选择一页显示多少行
     handleSizeChange(val) {
       console.log(`每页 ${val} 条`)
